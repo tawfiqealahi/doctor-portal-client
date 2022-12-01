be17c5f363dc67ba700192895b407afdbde51cbd
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-02 05:58:21 +0600

    route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Navbar from "./page/Home Page/shared/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
