4007f524f6b8a1f8f0ee7061cf3dcec044715afd
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-02 13:50:35 +0600

    menuItem

--- src/page/Home Page/shared/Navbar.js
+++ src/page/Home Page/shared/Navbar.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 const Navbar = () => {
+  const menuItem = <>
+      <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Appointment</a>
+            </li>
+            <li>
+              <a>Review</a>
+            </li>
+            <li>
+              <a>Contact us</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Login</a>
+            </li>
+  </>
   return (
     <div class="navbar bg-base-100">
       <div class="navbar-start">
@@ -25,58 +45,14 @@ const Navbar = () => {
             tabindex="0"
             class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Appointment</a>
-            </li>
-            <li>
-              <a>Review</a>
-            </li>
-            <li>
-              <a>Contact us</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Login</a>
-            </li>
+            {menuItem}
           </ul>
         </div>
         <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
       </div>
       <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal p-0">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabindex="0">
-            <a>
-              Parent
-              <svg
-                class="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul class="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+       {menuItem}
         </ul>
       </div>
       <div class="navbar-end">
