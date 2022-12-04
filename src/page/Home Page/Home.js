8659e20296f4c47ac2415597bf0fd1fa14f58bd0
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-05 01:20:52 +0600

    footer

--- src/page/Home Page/Home.js
+++ src/page/Home Page/Home.js
@@ -5,17 +5,19 @@ import Info from "./Info";
 import MakeAppointment from "../MakeAppointment/MakeAppointment";
 import Testimonial from "../Testimonials/Testimonial";
 import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
 
 
 const Home = () => {
   return (
-    <div className="px-7">
+    <div className="px-5">
       <Banner></Banner>
       <Info></Info>
       <Service></Service>
       <MakeAppointment></MakeAppointment>
       <Testimonial></Testimonial>
       <Contact></Contact>
+      <Footer></Footer>
       
     </div>
   );
