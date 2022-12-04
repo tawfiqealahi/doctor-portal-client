f2913411344952465e939435fcda13a76d807cca
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-05 02:27:02 +0600

    full responsive

--- src/page/Home Page/Banner.js
+++ src/page/Home Page/Banner.js
@@ -4,10 +4,13 @@ import chair from "../../images/chair.png";
 
 const Banner = () => {
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200 ">
+    
+      <div className="hero lg:min-h-screen bg-base-200 ">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+          <img
+            src={chair}
+            className="max-w-sm hidden lg:block rounded-lg shadow-2xl"
+          />
           <div>
             <h1 className="text-5xl font-bold">Box Office News!</h1>
             <p className="py-7 ">
@@ -19,7 +22,7 @@ const Banner = () => {
           </div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
