de970242650cc91c6bade1daabe053fd9df1c85d
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-04 16:03:34 +0600

    makeAppointment section heading edit

--- src/page/MakeAppointment/MakeAppointment.js
+++ src/page/MakeAppointment/MakeAppointment.js
@@ -6,12 +6,12 @@ const MakeAppointment = () => {
     return (
         <section style={{
             background:`url(${appointmentBackground})`
-        }} className='flex'>
+        }} className='flex my-28 justify-center items-center '>
             <div className='flex-1'>
                 <img src={doctor} alt="img" />
             </div>
-            <div className='flex-1 text-center '>
-                <h3 className="text-xl text-primary">Appointment</h3>
+            <div className='flex-1 text-center  '>
+                <h3 className="text-4xl  py-4 font-extrabold text-white   ">Appointment</h3>
                 <h2 className='text-3xl'>Make an AppointmentToday </h2>
                 <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem unde veritatis numquam. Quibusdam nesciunt officia laboriosam ipsa rem magni fugit? </p>
             </div>
