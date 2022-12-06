a6d9e538a7135c09078c9ad563ca9bc7f3565246
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-07 00:46:36 +0600

    available appointmrents date json

--- src/page/Appointment/AvailableAppointments.js
+++ src/page/Appointment/AvailableAppointments.js
@@ -1,10 +1,23 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const AvailableAppointments = ({date}) => {
+    const [services, setServices]=useState([]);
+
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res=>res.json())
+        .then(date=>setServices(date));
+    },[])
+
     return (
         <div>
             <p className='text-center pt-7'>Available Appointment on:  {format(date,'PP')}</p>
+            <div>
+                
+            </div>
         </div>
     );
 };
