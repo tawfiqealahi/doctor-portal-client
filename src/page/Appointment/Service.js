72946b36ec9757406dba85542458f34773781b78
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-12-08 19:02:52 +0600

    available appointmrents card button

--- src/page/Appointment/Service.js
+++ src/page/Appointment/Service.js
@@ -4,12 +4,19 @@ const Service = ({service}) => {
     const {name,slots}=service;
     return (
         <div>
-            <div class="card w-screen-sm bg-primary text-primary-content">
+            <div class="card w-screen-sm lg:max-w-lg bg-primary text-primary-content text-center">
   <div class="card-body">
     <h2 class="card-title">{name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <p>{
+        slots.length > 0
+         ? 
+        <span>{slots[0]}</span>
+        :
+        <span>No slot available</span>
+        }</p>
+    <p>{slots.length} {slots.length>1? 'spaces' : "space"}Unavailable. try another date </p>
     <div class="card-actions justify-end">
-      <button class="btn">Buy Now</button>
+      <button disabled={slots.length===0} class="btn text-white">Buy Now</button>
     </div>
   </div>
 </div>
